59b992f5e47556d3be34c32313216af1a265144d
Michal Malík <noonesvk@gmail.com>
2018-10-20 19:32:45 +0200

    Add file encryption/decryption and CLI

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+    "errors"
     "crypto/aes"
     "crypto/cipher"
     "crypto/rand"
     "golang.org/x/crypto/scrypt"
     "fmt"
+    //"io"
+    "os"
+    "io/ioutil" // reading/writing files
+    "time" // timing encryption/decryption
+    "flag" // command line flags
 )
 
 // Sizes are in bytes
@@ -20,10 +26,15 @@ const kAesNonceSize = 12
 //      r = 8
 //      p = 1
 const kScryptSaltSize = 32
-// N parameter, 2 << X
-const kScryptParamN = 10
+const kScryptParamN = 20 // 2 << X
 const kScryptParamR = 8
 const kScryptParamP = 1
+
+
+var (
+    ErrorEncrypt = errors.New("Encryption failed")
+    ErrorDecrypt = errors.New("Decryption failed")
+)
 
 
 func bzero(arr []byte) {
@@ -44,7 +55,8 @@ func deriveKey(password, salt []byte) *[kAesKeySize]byte {
     // func Key(password, salt []byte, N, r, p, keyLen int) ([]byte, error)
     key, err := scrypt.Key(password, salt, 2 << kScryptParamN, kScryptParamR, kScryptParamP, kAesKeySize)
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil
     }
 
     copy(finalKey[:], key)
@@ -52,20 +64,23 @@ func deriveKey(password, salt []byte) *[kAesKeySize]byte {
     return finalKey
 }
 
-func encrypt(key *[kAesKeySize]byte, plain_data []byte) []byte {
+func encrypt(key *[kAesKeySize]byte, plain_data []byte) ([]byte, error) {
     block, err := aes.NewCipher(key[:])
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorEncrypt
     }
 
     nonce, err := generateRandomBytes(kAesNonceSize)
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorEncrypt
     }
 
     aesgcm, err := cipher.NewGCM(block)
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorEncrypt
     }
 
     encrypted_data := aesgcm.Seal(nil, nonce, plain_data, nil)
@@ -73,18 +88,20 @@ func encrypt(key *[kAesKeySize]byte, plain_data []byte) []byte {
     // Prepend the nonce before the encrypted data so we can retrieve it later
     encrypted_data = append(nonce, encrypted_data...)
 
-    return encrypted_data
+    return encrypted_data, nil
 }
 
-func decrypt(key *[kAesKeySize]byte, encrypted_data []byte) []byte {
+func decrypt(key *[kAesKeySize]byte, encrypted_data []byte) ([]byte, error) {
     block, err := aes.NewCipher(key[:])
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorDecrypt
     }
 
     aesgcm, err := cipher.NewGCM(block)
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorDecrypt
     }
 
     nonce := make([]byte, kAesNonceSize)
@@ -92,19 +109,131 @@ func decrypt(key *[kAesKeySize]byte, encrypted_data []byte) []byte {
 
     data, err := aesgcm.Open(nil, nonce, encrypted_data[kAesNonceSize:], nil)
     if err != nil {
-        panic(err.Error())
+        fmt.Println(err.Error())
+        return nil, ErrorDecrypt
     }
 
-    return data
+    return data, nil
+}
+
+func Seal(password, plain_data []byte) ([]byte, error) {
+    salt, err := generateRandomBytes(kScryptSaltSize)
+    if err != nil {
+        fmt.Println(err.Error())
+        return nil, ErrorEncrypt
+    }
+
+    key := deriveKey(password, salt)
+    encrypted_data, err := encrypt(key, plain_data)
+    bzero(key[:])
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return nil, ErrorEncrypt
+    }
+
+    // Prepend encrypted data with salt used to derive the key
+    encrypted_data = append(salt, encrypted_data...)
+    return encrypted_data, nil
+}
+
+func Open(password, encrypted_data []byte) ([]byte, error) {
+    // Extract salt from the start of the file
+    salt := make([]byte, kScryptSaltSize)
+    copy(salt, encrypted_data[:kScryptSaltSize])
+
+    key := deriveKey(password, salt)
+    plain_data, err := decrypt(key, encrypted_data[kScryptSaltSize:])
+    bzero(key[:])
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return nil, ErrorDecrypt
+    }
+
+    return plain_data, nil
+}
+
+func EncryptFile(password []byte, in_path string, out_path string) error {
+    data, err := ioutil.ReadFile(in_path)
+    if err != nil {
+        return err
+    }
+
+    encrypted_data, err := Seal(password, data)
+    if err != nil {
+        return err
+    }
+
+    return ioutil.WriteFile(out_path, encrypted_data, 0644)
+}
+
+func DecryptFile(password []byte, in_path string, out_path string) error {
+    encrypted_data, err := ioutil.ReadFile(in_path)
+    if err != nil {
+        return err
+    }
+
+    plain_data, err := Open(password, encrypted_data)
+    if err != nil {
+        return err
+    }
+
+    return ioutil.WriteFile(out_path, plain_data, 0644)
 }
 
 func main() {
-    salt, _ := generateRandomBytes(kScryptSaltSize)
-    key := deriveKey([]byte("qwerty"), salt)
+    /*
+    Example 1: text encryption/decryption with a pre-defined password
 
-    ciphertext := encrypt(key, []byte("Hello encrypted world"))
-    fmt.Printf("%x\n", ciphertext)
 
-    plaintext := decrypt(key, ciphertext)
-    fmt.Printf("%s\n", plaintext)
+    ciphertext, err := Seal([]byte("qwerty"), []byte("Hello encrypted world"))
+    if err != nil {
+        panic(err.Error())
+    }
+
+    fmt.Printf("ciphertext: %x\n", ciphertext)
+
+    plaintext, err := Open([]byte("qwerty"), ciphertext)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    fmt.Printf("plaintext: %s\n", plaintext)
+    */
+
+    /*
+    Example 2: file encryption/decryption with a pre-defined password
+    */
+    flag.Usage = func() {
+        fmt.Fprintf(os.Stderr, "Usage: %s [-d|-e] <in_file> <out_file>\n", os.Args[0])
+    }
+
+    decryptFlag := flag.Bool("d", false, "decrypt <in_file> into <out_file>")
+    encryptFlag := flag.Bool("e", false, "encrypt <in_file> into <out_file>")
+    flag.Parse()
+
+    if flag.NArg() < 2 {
+        flag.Usage()
+        return
+    }
+
+    password := []byte("pleasedontstealourfiles")
+
+    start := time.Now()
+
+    if *decryptFlag && !*encryptFlag {
+        err := DecryptFile(password, flag.Arg(0), flag.Arg(1))
+        if err != nil {
+            panic(err.Error())
+        }
+    } else if *encryptFlag && !*decryptFlag {
+        err := EncryptFile(password, flag.Arg(0), flag.Arg(1))
+        if err != nil {
+            panic(err.Error())
+        }
+    }
+
+    elapsed := time.Since(start)
+    fmt.Printf("Took %s\n", elapsed)
 }
